2aa64794930313f970a24109b188d4180a6705dd
TimBiden <timbiden@gmail.com>
2017-10-11 14:12:32 -0700

    Separate functions

--- Algorithms Sorting/HeapSort2.rb
+++ Algorithms Sorting/HeapSort2.rb
@@ -3,37 +3,37 @@ def heapSort(array)
 
   length = array.length
 
-  def heap(array, length)
-    parent = length / 2
-    adjParent = parent - 1
-    right = parent * 2
-    left = right - 1
-    swap = false
-
-    while parent >= 0
-
-      if array[right].nil?
-        greater = left
-      elsif array[right] > array[left]
-        greater = right
-      else
-      greater = left
-      end
-
-      if array[greater] > array[parent]
-        array[greater], array[parent] = array[parent], array[greater]
-        swap = true
-      end
-
-      parent -= 1
-      puts array.to_s
-
-      length -= 1
-      heap(array, length)
-    end
-  end
-
   heap(array, length)
+end
+
+def heap(array, length)
+  parent = length / 2
+  adjParent = parent - 1
+  right = parent * 2
+  left = right - 1
+  swap = false
+
+  while parent >= 0
+
+    greater = if array[right].nil?
+                left
+              elsif array[right] > array[left]
+                right
+              else
+                left
+              end
+
+    if array[greater] > array[parent]
+      array[greater], array[parent] = array[parent], array[greater]
+      swap = true
+    end
+
+    parent -= 1
+    puts array.to_s
+
+    length -= 1
+    heap(array, length)
+  end
 end
 
 array = [0, 5, 1, 4, 3, 6, 9, 8, 2, 7]
