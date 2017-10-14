c38521de4f90ec7d3dbf7e17bbacaf6fa443e142
TimBiden <timbiden@gmail.com>
2017-10-14 13:58:43 -0700

    Get Diff

--- Algorithms Sorting/BucketSort.rb
+++ Algorithms Sorting/BucketSort.rb
@@ -12,6 +12,24 @@ def minMax(array)
       max = i
     end
   end
+
+  for i in len.downto(0)
+    puts "array[#{i}] = #{array[i]}"
+    if array[i] < array[min]
+      min = i
+    end
+  end
+
+  puts "min position = #{min}"
+  puts "min value = #{array[min]}"
+  puts "min: #{min}, max: #{max}"
+
+  getDif(array, min, max)
+end
+
+def getDif(array, min, max)
+  dif = max - min
+  blocks = dif / 5
 end
 
 array = [0.33, 0.45, 0.11, 0.4, 0.13, 0.66, 0.89, 0.80, 0.28, 0.37, 0.13]
